d7bf3b530790e4eabe75e8c1adea91182147a6ef
feamagno <feamagno2.fm@gmail.com>
2026-05-29 15:41:41 +0100

    linebreak

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -50,6 +50,10 @@ vim.keymap.set('n', 'zC', function()
   vim.cmd('normal! zs' .. math.floor(win_width / 2) .. 'zh')
 end, { desc = "Center cursor horizontally" })
 
+--  move up and down in line break
+vim.keymap.set('n', 'j', 'gj', { noremap = true, silent = true })
+vim.keymap.set('n', 'k', 'gk', { noremap = true, silent = true })
+
 --vim.keymap.set(
 --    "n",
 --    "<leader>ee",
