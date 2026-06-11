b7db59e642490e68068f0095b61f38b59b00a45e
feamagno <feamagno2.fm@gmail.com>
2026-06-11 14:32:37 +0100

    treesitter witchery

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,39 +1,23 @@
 return {
    "nvim-treesitter/nvim-treesitter",
-   branch = 'master', 
-   lazy = false, 
+   branch = "main",
+   lazy = false,
    build = ":TSUpdate",
    config = function()
-       require("nvim-treesitter.configs").setup ({
-           -- A list of parser names, or "all" (the listed parsers MUST always be installed)
+       require("nvim-treesitter").setup({
            ensure_installed = {
-               "javascript", "java", "c", "lua", "vim", "vimdoc", "query", 
-               "markdown", "markdown_inline", "python"
+               "javascript", "java", "c", "lua", "vim", "vimdoc", "query",
+               "markdown", "markdown_inline", "python",
            },
-
-           -- Install parsers synchronously (only applied to `ensure_installed`)
            sync_install = false,
-
-           -- Automatically install missing parsers when entering buffer
-           -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
            auto_install = true,
-
-           ---- If you need to change the installation directory of the parsers (see -> Advanced Setup)
-           -- parser_install_dir = "/some/path/to/store/parsers", -- Remember to run vim.opt.runtimepath:append("/some/path/to/store/parsers")!
-
-           highlight = {
-             -- 'false' will disable the whole extension
-             enable = true,
-
-             -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-             -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-             -- Using this option may slow down your editor, and you may see some duplicate highlights.
-             -- Instead of true it can also be a list of languages
-             additional_vim_regex_highlighting = false,
-           },
-           indent = {
-               enable = true
-           },
        })
-   end
+
+       vim.api.nvim_create_autocmd("FileType", {
+           callback = function()
+               pcall(vim.treesitter.start)
+               vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+           end,
+       })
+   end,
 }
